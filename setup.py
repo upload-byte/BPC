c30135c11e3877134bafdff53e80674eb948aa1c
upload-byte <a_mancini.live@protonmail.com>
2026-09-17 06:54:49 +0800

    BPCL

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="bpcl",
+    version="0.1.0",
+    description="System and network status utilities",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
