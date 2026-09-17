44976a6bdf58f5152fe9a4963107eb223b3c4eec
upload-byte <a_mancini.live@protonmail.com>
2026-09-17 20:22:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="bpcl",
+    name="BPCL",
     version="0.1.0",
     packages=find_packages(),  # Automatically finds the 'BPCL' folder
 )
